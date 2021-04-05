0a23294eaabad730214d2fca5228277e522f36de
LaurentLogati <64102719+LaurentLogati@users.noreply.github.com>
2021-04-05 19:06:54 +0200

    MEP_01.4
    Ajout bouton confirmer devis client sans mail
    Pop-up confirmation entrepôt demande de prix
    Prise en compte entrepôt devis client lors de la création demande de prix sous-traitance
    Correction statut attente de prix devis sous-traitance
    Prise en compte des sections et notes dans les demandes de prix

--- XenonVente/Xenon_purchase.py
+++ XenonVente/Xenon_purchase.py
@@ -36,38 +36,45 @@ class XenonPurchaseOrder(models.Model):
             
         # 1- Mise à jour de la liste de prix fournisseur
             for line in self.order_line:
-                # Do not add a contact as a supplier
-                partner = self.partner_id if not self.partner_id.parent_id else self.partner_id.parent_id
-                if line.product_id and partner in line.product_id.seller_ids.mapped('name'):              
-                    # Convert the price in the right currency.
-                    currency = partner.property_purchase_currency_id or self.env.company.currency_id
-                    price = self.currency_id._convert(line.price_unit, currency, line.company_id, line.date_order or fields.Date.today(), round=False)
-                    if line.product_id.product_tmpl_id.uom_po_id != line.product_uom:
-                        default_uom = line.product_id.product_tmpl_id.uom_po_id
-                        price = line.product_uom._compute_price(price, default_uom)
-                    self.env['product.supplierinfo'].search([('name','=',partner.id),('product_tmpl_id','=',line.product_id.product_tmpl_id.id)]).update({'price': price})
-                else:
-                    raise UserError(_(str(partner.name) + " n''est pas associé au produit %s. Veuillez définir ce fournisseur dans la liste des prix des fournisseurs du produit.") % (line.product_id.display_name,))
+                # On ne tient pas compte des lignes de type section ou note
+                if line.product_id.display_name != False:
+                    # Do not add a contact as a supplier
+                    partner = self.partner_id if not self.partner_id.parent_id else self.partner_id.parent_id
+                    if line.product_id and partner in line.product_id.seller_ids.mapped('name'):
+                        # Convert the price in the right currency.
+                        currency = partner.property_purchase_currency_id or self.env.company.currency_id
+                        price = self.currency_id._convert(line.price_unit, currency, line.company_id, line.date_order or fields.Date.today(), round=False)
+                        if line.product_id.product_tmpl_id.uom_po_id != line.product_uom:
+                            default_uom = line.product_id.product_tmpl_id.uom_po_id
+                            price = line.product_uom._compute_price(price, default_uom)
+                        self.env['product.supplierinfo'].search([('name','=',partner.id),('product_tmpl_id','=',line.product_id.product_tmpl_id.id)]).update({'price': price})
+                    else:
+                        raise UserError(_(str(partner.name) + " n''est pas associé au produit %s. Veuillez définir ce fournisseur dans la liste des prix des fournisseurs du produit.") % (line.product_id.display_name,))
                         
-        # 2- Mise à jour du prix de l'article dans le devis client 
-        # le process a été modifié pour avoir une seule demande de prix frs pour un devis client cf Xenon_purchase_stock_rule
-        # TODO recherche de la liste pourcentage en fonction des dates
-        # TODO mise à jour du prix de vente dans la fiche article
+                                                                    
+                                                                                                                                
+                                                                      
+                                                                  
 
-                rec_liste=self.env['sale.order'].search([('name','=', self.origin)])
-                for commande in rec_liste:
-                    lignecommande=commande.env['sale.order.line'].search([('order_id','=',commande.id)]) ###('display_type','=','')
-                    pxtousok=0
-                    for ligne in lignecommande:
-                        if ligne.x_px_maj==False and not ligne.display_type:
-                            pxtousok=pxtousok+1
-                            if ligne.product_id==line.product_id and partner in line.product_id.seller_ids.mapped('name'):
-                                pourcentage=self.env['x_calculprix.detail'].search([('x_max','>=',price),('x_min','<=',price)]).x_marge
-                                ligne.update({'price_unit':price * (1 + (pourcentage/100)), 'x_px_maj':True})
-                                pxtousok=pxtousok-1
-                                # Mise à jour du prix de vente dans la fiche article
-                                self.env['product.template'].search([('id','=',line.product_id.product_tmpl_id.id)]).update({'list_price': price * (1 + (pourcentage/100))})
-            
+            # 2- Mise à jour du prix de l'article dans le devis client 
+            # le process a été modifié pour avoir une seule demande de prix frs pour un devis client cf Xenon_purchase_stock_rule
+            # TODO recherche de la liste pourcentage en fonction des dates
+            # TODO mise à jour du prix de vente dans la fiche article
+
+                    rec_liste=self.env['sale.order'].search([('name','=', self.origin)])
+                    for commande in rec_liste:
+                        lignecommande=commande.env['sale.order.line'].search([('order_id','=',commande.id)]) ###('display_type','=','')
+                        pxtousok=0
+                        for ligne in lignecommande:
+                            if ligne.x_px_maj==False and not ligne.display_type:
+                                pxtousok=pxtousok+1
+                                if ligne.product_id==line.product_id and partner in line.product_id.seller_ids.mapped('name'):
+                                    pourcentage=self.env['x_calculprix.detail'].search([('x_max','>=',price),('x_min','<=',price)]).x_marge
+                                    ligne.update({'price_unit':price * (1 + (pourcentage/100)), 'x_px_maj':True})
+                                    pxtousok=pxtousok-1
+                                    # Mise à jour du prix de vente dans la fiche article
+                                    self.env['product.template'].search([('id','=',line.product_id.product_tmpl_id.id)]).update({'list_price': price * (1 + (pourcentage/100))})
+
             #Changement du statut du devis client A envoyer
             if pxtousok==0:
                 commande.write({'state': 'tosend'})
