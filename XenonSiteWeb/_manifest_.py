784d4f0f2081cbd5f554c4219be2b7490965bb05
LaurentLogati <64102719+LaurentLogati@users.noreply.github.com>
2022-01-22 12:25:29 +0100

    test

--- XenonSiteWeb/_manifest_.py
+++ XenonSiteWeb/_manifest_.py
@@ -4,7 +4,7 @@
 	'author':"Laurent LOEZIC",
     'category': 'Site',
     'summary': 'Site web Xenon',
-	'depends':['web', 'website', 'theme_treehouse', 'theme_common'],
+    'depends':['sale', 'sale_purchase', 'sale_stock', 'purchase', 'product', 'website', 'portal','web', 'theme_treehouse', 'theme_common'],
     'data': ['views/theme_xenon.xml', 'views/xenon_layout.xml'],
     'css' : ['static/src/css/theme_xenon.scss'],
 	'demo':[],
