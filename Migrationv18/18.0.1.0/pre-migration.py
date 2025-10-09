1c24d4c9e1c5a6d6fc7aed67a66a53954c9c5aee
LaurentLogati <64102719+LaurentLogati@users.noreply.github.com>
2025-10-09 23:19:53 +0200

    ajout de xenoncompta dans premigration

--- Migrationv18/18.0.1.0/pre-migration.py
+++ Migrationv18/18.0.1.0/pre-migration.py
@@ -6,6 +6,7 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     _logger.info('Start script for deactivating module')
     _logger.info(f'Version: {version}')
+    cr.execute("DELETE FROM ir_module_module WHERE name ILIKE 'XenonCompta'")
     cr.execute("DELETE FROM ir_module_module WHERE name ILIKE 'account_financial_report'")
     cr.execute("DELETE FROM ir_module_module WHERE name ILIKE 'date_range'")
     cr.execute("DELETE FROM ir_module_module WHERE name ILIKE 'report_xlsx'")
